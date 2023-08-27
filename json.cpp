a8a7c35c06f67ee72befe967809670762bb63dc9
jacob-nnc <2507689916@qq.com>
2023-08-27 11:44:35 +0800

    源文件

---
+++ json.cpp
@@ -0,0 +1,208 @@
+#include "value.h"
+using namespace std;
+
+#define error(message)                \
+	{cout << string("error ")+message << endl; \
+	exit(1);}
+
+struct json2struct
+{
+	using value = json::value;
+
+	string json_str;
+
+	json2struct(string filename)
+	{
+		json_str = chuli(getCode(filename));
+	}
+
+	json::obj* from()
+	{
+		return make_obj(json_str.substr(1,json_str.size()-2));
+	}
+	
+private:
+	value* make_val(string _t)
+	{
+		value* _val = nullptr;
+		
+		if (_t == "null")
+		{
+			_val = new json::null();
+		}
+		else if (_t == "false")
+		{
+			_val = new json::j_bool(0);
+		}
+		else if (_t == "true")
+		{
+			_val = new json::j_bool(1);
+		}
+		else if (_t[0] == '"')
+		{
+			_val = new json::str(_t.substr(1,_t.size()-2));
+		}
+		else if (_t[0] == '{')
+		{
+			_val = make_obj(_t.substr(1, _t.size() - 2));
+		}
+		else if (_t[0] == '[')
+		{
+			_val = make_arr(_t.substr(1, _t.size() - 2));
+		}
+		else if (isdigit(_t[0]))
+		{
+			if (_t.find('.') != -1)
+			{
+				_val = new json::num<double>(stod(_t));
+			}
+			else
+			{
+				_val = new json::num<int>(stoi(_t));
+			}
+		}
+		else
+		{
+			error("Strange value");
+		}
+
+		return _val;
+	}
+	string chuli(string _val)
+	{
+		auto noeff_char = string(" \n\t");
+		string ret;
+		bool flag = 0;
+		for (auto i : _val)
+		{
+			if (i == '"')flag = !flag;
+			if (flag || noeff_char.find(i) == -1)
+			{
+				ret += i;
+			}
+		}
+		return ret;
+	}
+	string getCode(string filename)
+	{
+		return (stringstream() << fstream(filename).rdbuf()).str();
+	}
+	json::obj *make_obj(string json_str)
+	{
+		map<string, value*> temp_data;
+
+		const string spa_char = "{}[]\":,";
+		string key;
+		string _str;
+		int i = 0;
+		int _t = 0;
+
+		enum
+		{
+			KEY = 0,
+			VAL,
+		};
+
+		int flag = KEY;
+
+		json::value *_val=nullptr;
+		for (; i != json_str.size();)
+		{
+			if (json_str[i] == ':')
+			{
+				flag = VAL;
+				i++;
+			}
+			else if (json_str[i] == ',')
+			{
+				temp_data[key] = _val;
+				flag = KEY;
+				key="";
+				_val = nullptr;
+				i++;
+			}
+
+			if (flag == KEY)
+			{
+				int j = i;
+				for (; json_str[i] != ':'; i++);
+				key = json_str.substr(j + 1, i - j - 2);
+			}
+			else
+			{
+				int j = i;
+				if (json_str[j] == '{' || json_str[j] == '[')
+				{
+					int _t = 0;
+					for (;; j++)
+					{
+						if (json_str[i] == json_str[j])
+						{
+							_t++;
+						}
+						else if (json_str[i] + 2 == json_str[j]) {
+							_t--; if (_t == 0)break;
+						}
+					}
+					j++;
+				}
+				else
+				{
+					for (; json_str[j] != ',' && j != json_str.size(); j++);
+				}
+				_val = make_val(json_str.substr(i, j - i));
+				i = j;
+			}
+		}
+		temp_data[key] = _val;
+		return new json::obj(temp_data);
+	}
+	json::arr *make_arr(string json_str)
+	{
+		auto ret = new json::arr();
+
+		string _str;
+		int i = 0;
+		int _t = 0;
+
+		json::value *_val;
+
+		for (;i!=json_str.size();)
+		{
+			if (json_str[i] == ',')
+			{
+				ret->val.push_back(_val);
+				_val = nullptr;
+				i++;
+			}
+			else
+			{
+				int j = i;
+				if (json_str[j] == '{' || json_str[j] == '[')
+				{
+					int _t = 0;
+					for (;j< json_str.size(); j++)
+					{
+						if (json_str[i] == json_str[j])
+						{
+							_t++;
+						}
+						else if (json_str[i] + 2 == json_str[j]) {
+							_t--; if (_t == 0)break;
+						}
+					}
+					j++;
+				}
+				else
+				{
+					for (; json_str[j] != ',' && j!=json_str.size(); j++);
+				}
+				_val = make_val(json_str.substr(i, j - i));
+				i = j;
+			}
+		}
+		ret->val.push_back(_val);
+		return ret;
+	}
+};
+
---
+++ value.h
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <unordered_map>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+namespace json
+{
+    struct value
+    {
+        virtual int type() = 0;
+        virtual operator std::string() = 0;
+        virtual ~value() = 0;
+    protected:
+        static const std::string _type[6];
+    };
+
+    value::~value() {}
+    const std::string value::_type[6] = { "null","bool","num", "str", "arr", "obj"};
+
+    enum {
+        j_NULL,
+        j_BOOl,
+        j_NUM,
+        j_STR,
+        j_ARR,
+        j_OBJ,
+    };
+
+    struct str : public value
+    {
+        std::string val;
+
+        str() = default;
+        str(std::string _v) :val(_v) {}
+        ~str() {}
+
+        operator std::string()
+        {
+            return '"' + val + '"';
+        }
+
+        int type()
+        {
+            return 3;
+        }
+
+
+    };
+
+    template <class name>
+    struct num : public value
+    {
+        using T = typename std::enable_if<std::is_same_v<name, int> || std::is_same_v<name, double>, name>::type;
+        T val;
+
+        num() = default;
+        num(T _v) :val(_v) {}
+        ~num() {};
+
+
+        operator std::string()
+        {
+            return std::to_string(val);
+        }
+
+        operator T()
+        {
+            return val;
+        }
+
+        int type()
+        {
+            return 2;
+        }
+    };
+
+    struct obj : public value
+    {
+        std::map<std::string,  value* > val;
+
+        obj() = default;
+        obj(const std::map<std::string, value*>& _v) :val(_v) {}
+        ~obj() {
+            for (auto i : val)
+            {
+                delete(i.second);
+            }
+        }
+
+        auto operator[](const std::string& index)
+        {
+            return val[index];
+        }
+
+        operator std::string()
+        {
+            std::stringstream _t;
+            _t << '{';
+            for (auto i = val.begin(); i != --val.end(); i++)
+            {
+                _t << '"' + i->first + '"' << ':' << std::string(*i->second) << ',';
+            }
+            _t << '"' + val.rbegin()->first + '"' << ':' << std::string(*val.rbegin()->second) << '}';
+            return _t.str();
+        }
+
+        int type()
+        {
+            return 5;
+        }
+    };
+
+    struct arr : public value
+    {
+        std::vector<value*> val;
+
+        arr() = default;
+        arr(const std::vector<value*>& _v) :val(_v) {}
+        ~arr() {
+            for (auto i : val)
+            {
+                delete (i);
+            }
+        }
+
+        auto operator [](size_t index)
+        {
+            return val[index];
+        }
+
+        operator std::string()
+        {
+            std::stringstream _t;
+            _t << '[';
+            for (auto i = val.begin(); i != --val.end(); i++)
+            {
+                _t << (std::string) * *i << ',';
+            }
+            _t << (std::string)*val.back() << ']';
+            return _t.str();
+        }
+
+        int type()
+        {
+            return 4;
+        }
+    };
+
+    struct j_bool :public value
+    {
+        bool val;
+
+        j_bool() = default;
+        j_bool(bool _val) :val(_val) {}
+        ~j_bool() {}
+
+        operator bool()
+        {
+            return val;
+        }
+
+        operator std::string()
+        {
+            return val ? "true" : "false";
+        }
+
+        int type()
+        {
+            return 1;
+        }
+    };
+
+    struct null : public value
+    {
+
+        null() = default;
+        ~null() {};
+
+        operator bool() {
+            return false;
+        }
+
+        operator std::string()
+        {
+            return "null";
+        }
+
+        int type()
+        {
+            return 0;
+        }
+    };
+}
